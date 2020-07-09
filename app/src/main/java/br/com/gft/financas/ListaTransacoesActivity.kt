eaf002ba584a37244ee5f007cc58ec515d2e4219
leandro-gft <lpsacchi@gmail.com>
2020-07-09 15:16:44 -0300

    Utilizando o synthetic para buscar as views

--- app/src/main/java/br/com/gft/financas/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/gft/financas/ListaTransacoesActivity.kt
@@ -2,12 +2,22 @@ package br.com.gft.financas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$30,50", "Economia - R$100,00")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.adapter = arrayAdapter
+
+        // = findViewById<ListView>(R.id.lista_transacoes_listview)
+
     }
 }
